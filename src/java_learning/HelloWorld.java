a56b1c8a371cfc724a8557ae8c6f9b42650f1a46
“kieuvu” <kieuvuvuvu@gmail.com>
2022-08-30 11:58:49 +0700

    init java app

---
+++ src/java_learning/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package java_learning;
+
+/**
+ * @author kieuvu
+ *
+ * Aug 30, 2022
+ * 11:40:37 AM 
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.print("Hello World from Vukm");
+	}
+}	
---
+++ src/java_learning/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author kieuvu
+ *
+ * Aug 30, 2022
+ * 11:39:37 AM 
+ */
+package java_learning;
